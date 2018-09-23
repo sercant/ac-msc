d3983eccbc014b7c14cc2871055c016785a8e377
Sercan Turkmen <sercanturkmen@outlook.com>
2018-09-23 16:27:24 +0300

    task 1 completed?

--- Exercise 2/solution.py
+++ Exercise 2/solution.py
@@ -11,30 +11,35 @@ from scipy.signal import lfilter
 from f0Lib import getF0
 
 
-def resample(x, source_fs, target_fs):
-    return signal.resample(x, int(len(x) * target_fs / source_fs))
-
 # BEGIN TASK 0 #
 
 fig, ax = plt.subplots(nrows=4, ncols=1)
 
 # 1. Load the 'speech_sample'
-sample_speech = sio.loadmat('lab2_data.mat')
-
+exerciseData = sio.loadmat('lab2_data.mat')
+speechSample = exerciseData['speech_sample'].reshape(-1)
 # 2. Declare the source sampling frequency, and the target sampling frequency.
 #    2.1 Source sampling frequency
 fs_source = 48000
 
 #    2.2 Target sampling frequency
 # Target frequency
-fs_target = 11025
+fs_down = 11025
+
+speechLength = len(speechSample) / fs_source
 
 # #. Downsample the speech sample
-x = sample_speech['speech_sample'].ravel()
-x0 = resample(x, fs_source, fs_target)
+speech_resampled = signal.resample(speechSample, int(
+    np.round(speechLength * fs_down)))
 
-# Visualize the downsampled speech signal.
-ax[0].plot(x0)
+# 4. Visualize the downsampled speech signal.
+#    4.1 Creating the corresponding time vector, whose length is same to the length to the given signal.
+#        You can use np.linspace() function to perform this. For example
+
+#    4.2 Plot your result
+# ax[0].plot(speechSample)
+ax[0].plot(np.linspace(0, speechLength, len(speech_resampled)),
+           speech_resampled, label='Downsampled Signal')
 
 # END TASK 0 #
 
@@ -47,28 +52,40 @@ ax[0].plot(x0)
 #        filter coefficients b, which is the numerator
 #        filter coefficients a, which is the denominator
 coef = 0.98
-b = np.array([1., -coef], x0.dtype)
-a = np.array([1.], x0.dtype)
-x1 = signal.lfilter(b, a, x0)
+b = np.array([1., -coef], speech_resampled.dtype)
+a = np.array([1.], speech_resampled.dtype)
+preEmphasizedSample = signal.lfilter(b, a, speech_resampled)
 
-ax[0].plot(x1)
+ax[0].plot(np.linspace(0, speechLength, len(speech_resampled)),
+           preEmphasizedSample, label='Pre-emphasized Signal')
 
 # 2. Extract the mfcc coefficient by callying the mfcc() function
 # remeber to set the pre-emphasize argument to 0 since the signal has been pre-emphasized.
-frameLen = int(2 ** np.floor(np.log2(0.03 * fs_target)))
-mfccContour = mfcc(x1,
-                   samplerate=fs_target,
-                   winlen=float(frameLen)/fs_target,
-                   winstep=float(frameLen)/(2*fs_target),
+frameLen = int(2 ** np.floor(np.log2(0.03 * fs_down)))
+mfccContour = mfcc(preEmphasizedSample,
+                   samplerate=fs_down,
+                   winlen=float(frameLen)/fs_down,
+                   winstep=float(frameLen)/(2*fs_down),
                    numcep=12,
                    preemph=0)
 
-
 # 3. Plot the 12 mfcc contours
-ax[1].plot(mfccContour)
+for k in range(0, len(mfccContour)):
+    ax[1].plot(np.linspace(0, speechLength,
+                           len(mfccContour[k])), mfccContour[k])
 
 # 4. Calculate the mean for each contour.
-mean_mfccContour = np.mean(mfccContour, axis=tuple(range(mfccContour.ndim - 1)))
+mean_mfccContour = np.mean(
+    mfccContour, axis=tuple(range(mfccContour.ndim - 1)))
+
+# Why do we need to pre-emphasize the speech signal before computing the MFCC feature?
+
+# The first step is to apply a pre-emphasis filter on the signal to amplify the high
+# frequencies. A pre-emphasis filter is useful in several ways: (1) balance the frequency
+# spectrum since high frequencies usually have smaller magnitudes compared to lower
+# frequencies, (2) avoid numerical problems during the Fourier transform operation
+# and (3) may also improve the Signal-to-Noise Ratio (SNR).
+# Pre-emphasis often used for loudness equalization
 
 # END TASK 1.1 #
 
@@ -76,22 +93,22 @@ mean_mfccContour = np.mean(mfccContour, axis=tuple(range(mfccContour.ndim - 1)))
 
 # 1. Define a hamming window
 #    1.1 Calculate the window length, which the number of frames within 0.01s
-sample_count_per_frame = int(0.01 * fs_target)
-window_length = int(np.ceil(len(x0) / sample_count_per_frame))        # number of non-overlapping _full_ frames
+# number of non-overlapping _full_ frames
+window_length = int(np.round(0.01 * fs_down))
 
 #    1.2 Define the hamming window using signal.hamming()
 hamming_window = signal.windows.hamming(window_length)
 
 # 2. Calculate the short time energy (STE) contour by convolve the hamming window and the squared signal,
 #    using the scipy.signal.convolve() function
-x12 = signal.convolve(x0 ** 2, hamming_window)
+x12 = signal.convolve(speech_resampled ** 2, hamming_window)
 
 # 3. Clip half window of frames from both the beginning and end of the STE contour
-clip = int(np.ceil(sample_count_per_frame / 2))
+clip = int(np.ceil(window_length / 2))
 x12 = x12[clip:-clip]
 
 # 4. Visualize the final STE contour.
-ax[2].plot(x12)
+ax[2].plot(np.linspace(0, speechLength, len(x12)), x12)
 
 # 5. Calculate the 5 distribution parameter feature the of STE contour
 mean_ste = np.mean(x12)
@@ -104,24 +121,103 @@ kurtosis_ste = stats.kurtosis(x12)
 
 # BEGIN TASK 1.3 #
 
-# 1. Extract the F0 contour
-F0, strength, T_ind, wflag = getF0(x0, fs_target)
+# 1. Extract the f0 contour
+f0, _, T_ind, _ = getF0(speech_resampled, fs_down)
 
-# 2. Visualize the F0 contour
-ax[3].plot(F0)
+# 2. Visualize the f0 contour
+ax[3].plot(T_ind[:, 0] / fs_down, f0)
 
 # 3. Calculate these distribution parameter features
-mean_F0 = np.mean(F0)
-std_F0 = np.std(F0)
-ten_perc_F0 = np.percentile(F0, 10)
-ninty_perc_F0 = np.percentile(F0, 90)
-kurtosis_F0 = stats.kurtosis(F0)
+mean_f0 = np.mean(f0)
+std_f0 = np.std(f0)
+ten_perc_f0 = np.percentile(f0, 10)
+ninty_perc_f0 = np.percentile(f0, 90)
+kurtosis_f0 = stats.kurtosis(f0)
 
 # END TASK 1.3 #
 
 # BEGIN TASK 1.4 #
 
+# 1. Segmenting the voiced and unvoiced speech segements.
+#    1.1 Example on extracting voiced segment lengths
+framesInd_voiced = np.where(f0 > 0)[0]
+diff = framesInd_voiced[1:] - framesInd_voiced[0:-1]
+voiceToUnviceInd = np.where(diff > 1)[0]
+voice_seg_num = len(voiceToUnviceInd) + 1
+voice_seg_lengths = np.zeros(voice_seg_num)
+tmp = framesInd_voiced[0]
 
+for i in range(voice_seg_num - 1):
+    voice_seg_lengths[i] = framesInd_voiced[voiceToUnviceInd[i]] - tmp + 1
+    tmp = framesInd_voiced[voiceToUnviceInd[i] + 1]
+
+voice_seg_lengths[-1] = framesInd_voiced[-1] - tmp + 1
+
+#####################################################################
+###################################################################
+#    1.2 Extract unvoiced segment lengths.
+framesInd_unvoiced = np.where(f0 == 0)[0]
+diff = framesInd_unvoiced[1:] - framesInd_unvoiced[0:-1]
+ind = np.where(diff > 1)[0]
+unvoiced_seg_num = len(ind) + 1
+unvoiced_seg_lengths = np.zeros(unvoiced_seg_num)
+tmp = framesInd_unvoiced[0]
+
+for i in range(unvoiced_seg_num - 1):
+    unvoiced_seg_lengths[i] = framesInd_unvoiced[ind[i]] - tmp + 1
+    tmp = framesInd_unvoiced[ind[i] + 1]
+
+unvoiced_seg_lengths[-1] = framesInd_unvoiced[-1] - tmp + 1
+
+# 2. Calculate the means and SDs of both Voiced and Unvoiced segment lengths
+mean_voiced = np.mean(voice_seg_lengths)
+mean_unvoiced = np.mean(unvoiced_seg_lengths)
+
+std_voiced = np.std(voice_seg_lengths)
+std_unvoiced = np.std(unvoiced_seg_lengths)
+
+# 3. Calculate the voicing ratio.
+voicing_ratio = np.sum(voice_seg_lengths) / np.sum(unvoiced_seg_lengths)
+
+# END TASK 1.4 #
+
+# START TASK 1.5 #
+
+# 1. Print the 12 MFCC coefficients
+print('--- 12 MFCC coefficients ---')
+for k in range(0, len(mean_mfccContour)):
+    print('{}: {}'.format(k, mean_mfccContour[k]))
+print('')
+
+# 2. Print the distribution paremeter feature of the STE contour
+print('--- Distribution paremeters of the STE contour ---')
+print(
+    'mean_ste: ', mean_ste, '\n'
+    'std_ste: ', std_ste, '\n'
+    'ten_perc_ste: ', ten_perc_ste, '\n'
+    'ninty_perc_ste: ', ninty_perc_ste, '\n'
+    'kurtosis_ste: ', kurtosis_ste, '\n'
+)
+
+# 3. Print the distribution parameter feature of the F0 contour
+print('--- Distribution paremeters of the F0 contour ---')
+print(
+    'mean_f0: ', mean_f0, '\n'
+    'std_f0: ', std_f0, '\n'
+    'ten_perc_f0: ', ten_perc_f0, '\n'
+    'ninty_perc_f0: ', ninty_perc_f0, '\n'
+    'kurtosis_f0: ', kurtosis_f0, '\n'
+)
+
+# 3. Print the 5 prodosic features
+print('--- 5 prodosic features ---')
+print(
+    'mean_voiced: ', mean_voiced, '\n'
+    'mean_unvoiced: ', mean_unvoiced, '\n'
+    'std_voiced: ', std_voiced, '\n'
+    'std_unvoiced: ', std_unvoiced, '\n'
+    'voicing_ratio: ', voicing_ratio, '\n'
+)
 # END TASK 1.4 #
 
 # END TASK 1 #
